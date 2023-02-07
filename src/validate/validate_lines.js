7bd6fbd2ae4ecdad85d810098ac2113cd28697d9
MuffinPlease <benjamintec3455@gmail.com>
2023-02-07 11:15:55 -0600

    UPDATE valida_lines ids

--- src/validate/validate_lines.js
+++ src/validate/validate_lines.js
@@ -1,15 +1,14 @@
 function validate_lines(text) {
     //Ids
-    const NAME = /(?<name>[\w]+)/;
-    const TYPE = /(?<type>[\w]+)/;
-    const VALUE = /(?<value>.*)/;
-    const SPACE = / */;
-    const OPBLOCK = /(?<open>\(?)/;
-    const CLBLOCK = /(?<close>\)?)/;
+    const NAME = "(?<name>[\\w]+)";
+    const TYPE = "(?<type>[\\w]+)";
+    const VALUE = "(?<value>.*)";
+    const SPACE = " *";
+    const OPBLOCK = "(?<open>\\(?)";
+    const CLBLOCK = "(?<close>\\)?)";
 
     //Statement
-    const ASIGNATIONID =
-        NAME.source + SPACE.source + "=" + SPACE.source + VALUE.source;
+    const ASIGNATIONID = NAME + SPACE + "=" + SPACE + VALUE;
 
     const statements = [
         {
@@ -18,68 +17,63 @@ function validate_lines(text) {
         },
         {
             name: "declaration",
-            id: TYPE.source + SPACE.source + ASIGNATIONID,
+            id: TYPE + SPACE + ASIGNATIONID,
         },
         {
             name: "if",
             id:
                 "(?<start>&&&|\\|\\|\\|)" +
-                SPACE.source +
+                SPACE +
                 "\\(" +
-                VALUE.source +
+                VALUE +
                 "\\)" +
-                SPACE.source +
+                SPACE +
                 ":" +
-                SPACE.source +
+                SPACE +
                 "\\{" +
-                SPACE.source,
+                SPACE,
         },
         {
             name: "for",
             id:
-                NAME.source +
-                SPACE.source +
+                NAME +
+                SPACE +
                 "\\?" +
-                SPACE.source +
+                SPACE +
                 "\\(" +
-                VALUE.source +
+                VALUE +
                 "\\)" +
-                SPACE.source +
+                SPACE +
                 ":" +
-                SPACE.source +
+                SPACE +
                 "\\{" +
-                SPACE.source,
+                SPACE,
         },
         {
             name: "while",
             id:
                 "&&&" +
-                SPACE.source +
+                SPACE +
                 "\\(" +
-                VALUE.source +
+                VALUE +
                 "\\)" +
-                SPACE.source +
+                SPACE +
                 "\\?" +
-                SPACE.source +
+                SPACE +
                 ":" +
-                SPACE.source +
+                SPACE +
                 "\\{" +
-                SPACE.source,
+                SPACE,
         },
         {
             name: "output",
-            id:
-                "output" +
-                SPACE.source +
-                OPBLOCK.source +
-                VALUE.source +
-                SPACE.source +
-                CLBLOCK.source,
+            id: "output" + SPACE + OPBLOCK + VALUE + SPACE + CLBLOCK,
         },
         { name: "close", id: "}" },
     ];
 
     const lines = text.map((line, index) => {
+        if (!line) return null;
         for (const { name, id } of statements) {
             const match = RegExp(id).exec(line);
 
