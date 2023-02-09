9dcbca5456c5629b2737d8cd7af9eaf58ee3b510
MuffinPlease <benjamintec3455@gmail.com>
2023-02-09 17:09:19 -0600

    CREATE validate_sense & Manage_tokens row files

--- src/validate/Manage_tokens.js
+++ src/validate/Manage_tokens.js
@@ -1,29 +1,99 @@
 class Manage_tokens {
     constructor() {
         this.tokens = [];
+        this.validates = {
+            value: ({ type, id, value }) => {
+                if (!value.match(id))
+                    throw new Error(`${value} is an invalid ${type} value.`);
+                else return { value };
+            },
+            variable: ({ type, value }) => {
+                if (!this.exists(value))
+                    throw new Error(`variable ${value} no exist.`);
+
+                const token = this.find(value);
+                if (token.type != type)
+                    throw new Error(`${value} is an invalid ${type} value.`);
+                else return { value };
+            },
+            input: ({ type, open, close, value, input_type }) => {
+                if (!(open && close))
+                    throw new Error(
+                        `${!open ? "(" : ")"} is missing at input statement.`
+                    );
+
+                const value_id = '[\\w" ]+';
+
+                if (!value.match(value_id))
+                    throw new Error(`${value} is invalid value.`);
+
+                if (type != input_type)
+                    throw new Error(`${name} can't save ${type} value.`);
+
+                return { value };
+            },
+            math: ({ type, id, value_a, value_b, operator }) => {
+                //Values
+                const values = [value_a.trim(), value_b.trim()];
+
+                for (const value of values) {
+                    if (value.match(/[_A-Za-z][\w]*/)) {
+                        if (!this.exists(value))
+                            throw new Error(`variable ${value} no exist.`);
+
+                        const token = this.find(value);
+                        if (token.type != type)
+                            throw new Error(
+                                `${value} is an invalid ${type} value.`
+                            );
+                        continue;
+                    }
+                    if (value.match(/[\w" ]+/))
+                        if (!value.match(id))
+                            throw new Error(
+                                `${value} is an invalid ${type} value.`
+                            );
+                }
+
+                //Operator
+                const type_operators = {
+                    int: /\+|-|\*|\\|%|\^/,
+                    floant: /\+|-|\*|\/|\\|%|\^/,
+                    string: /\+|-/,
+                };
+
+                const id_operator = type_operators[type] || null;
+                if (!operator.match(id_operator))
+                    throw new Error(
+                        `Invalid operator ${operator} to ${type} type.`
+                    );
+
+                return {
+                    value_a: value_a.trim(),
+                    value_b: value_b.trim(),
+                    operator,
+                };
+            },
+        };
     }
 
     exists(name) {
         return this.tokens.some((token) => token.name == name);
     }
-    // create: function ({ type, name, operation, content }) {
+    find(name) {
+        return this.tokens.find((token) => token.name == name);
+    }
     create({ type, name, operation, content }) {
+        if (this.exists(name)) throw Error(`${name} already exist`);
         const type_id = {
             int: /^(-?\d+)$/,
-            floant: /^-?\d+.\d+?$/,
+            float: /^-?\d+(\.\d+)?$/,
             string: /^\".+\"$/,
         };
 
-        const validates = {
-            value: (id, { value }) => {
-                if (!value.match(id))
-                    throw new Error(`${value} is an invalid ${type} value.`);
-                else return { value };
-            },
-        };
         const id = type_id[type] || null;
-        const validate = validates[operation] || (() => {});
-        const value = validate(id, content);
+        const validate = this.validates[operation] || (() => {});
+        const value = validate({ type, id, ...content });
 
         if (value)
             this.tokens.push({
@@ -33,9 +103,26 @@ class Manage_tokens {
             });
     }
 
-    modify({ name, value }) {
-        const token = this.tokens.find((token) => token.name == name);
-        token.value = value;
+    modify({ name, operation, content }) {
+        if (!this.exists(name)) throw new Error(`variable ${name} no exist.`);
+        const token = this.find(name);
+
+        if (token.name == token.name.toUpperCase())
+            throw new Error(`${name} is a constant.`);
+
+        const type = token.type;
+
+        const type_id = {
+            int: /^(-?\d+)$/,
+            float: /^-?\d+(\.\d+)?$/,
+            string: /^\".+\"$/,
+        };
+
+        const id = type_id[type] || null;
+        const validate = this.validates[operation] || (() => {});
+        const value = validate({ type, id, ...content });
+
+        token.content = content;
     }
 }
 
--- src/validate/validate_sense.js
+++ src/validate/validate_sense.js
@@ -1,11 +1,94 @@
 const { Manage_tokens } = require("./Manage_tokens");
 
+const validate_conditions = (Manage, { evaluations }) => {
+    for (const { value_a, value_b } of evaluations) {
+        const tokens = [];
+        for (const value of [value_a.trim(), value_b.trim()]) {
+            if (value.match(/[_A-Za-z][\w]*/)) {
+                if (!Manage.exists(value))
+                    throw new Error(`variable ${value} no exist.`);
+
+                tokens.push(Manage.find(value).type);
+                continue;
+            }
+
+            if (value.match(/[\w" ]+/)) {
+                const types = [
+                    { name: "int", id: /^(-?\d+)$/ },
+                    { name: "float", id: /^-?\d+(\.\d+)?$/ },
+                    { name: "string", id: /^\".+\"$/ },
+                ];
+
+                const type = (() => {
+                    for (const { name, id } of types) {
+                        if (value.match(id)) {
+                            return name;
+                        }
+                    }
+                })();
+                if (!type) throw new Error(`${value} not identifiable.`);
+                else tokens.push(type);
+            }
+        }
+        if (new Set(tokens).size != 1)
+            throw new Error(
+                `${value_a.trim()} & ${value_b.trim()} don't match types.`
+            );
+    }
+    // { operation: "variable", id:  },
+    // { operation: "value", id: /[0-9]+/ },
+};
+
 function validate_sense(actions) {
     const Tokens = new Manage_tokens();
 
     const validates = {
-        // asignation: asignation_action,
-        declaration: (data) => Tokens.create(data),
+        declaration: (content) => Tokens.create(content),
+        asignation: (content) => Tokens.modify(content),
+        output: ({ operation, value }) => {
+            const validates = {
+                variable: (value) => {
+                    if (!Tokens.exists(value))
+                        throw new Error(`variable ${value} no exist.`);
+                },
+            };
+            const validate = validates[operation] || (() => {});
+            validate(value);
+        },
+        for: ({ name, value_a, value_b }) => {
+            if (Tokens.exists(name))
+                throw new Error(`variable ${name} already exist.`);
+
+            //verify value
+            const values = [];
+            for (const { operation, value } of [value_a, value_b]) {
+                if (operation === "variable") {
+                    if (!Tokens.exists(value))
+                        throw new Error(`variable ${value} no exist.`);
+
+                    const token = Tokens.find(value);
+
+                    if (token.type !== "int")
+                        throw new Error(`${token.name} must be int type.`);
+
+                    values.push(parseInt(token.value));
+                    continue;
+                }
+                if (operation === "value") {
+                    if (!value.match(/^(-?\d+)$/))
+                        throw new Error(`${value} must be int type.`);
+                    values.push(parseInt(value));
+                    continue;
+                }
+            }
+            // if (values[0] >= values[1])
+            //     throw new Error(
+            //         `${values[0]} must be lower than ${values[1]}.`
+            //     );
+        },
+        if: (content) => {
+            validate_conditions(Tokens, content);
+        },
     };
 
     for (const { index, name, content } of actions) {
@@ -17,7 +100,7 @@ function validate_sense(actions) {
             throw new Error(`Line ${index}: ${error.message}`);
         }
     }
-    console.log(Tokens.tokens);
+    // console.log(Tokens.tokens);
 }
 
 module.exports = { validate_sense };
